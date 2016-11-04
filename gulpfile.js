a8a6e7b412d9d68efd2de0642b215540703097f7
ozPop <ozziepopovas@gmail.com>
2016-11-04 14:30:32 -0400

    fixup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp    = require('gulp'),
     jasmine = require('gulp-jasmine-livereload-task');
 
 gulp.task('default', jasmine({
-    files: ['./src/**/*.js', './js/**/*.js', './lib/**/*.js', './spec/**/*.js']
+    files: ['./app/assets/**/*.js','./spec/**/*.js']
 }));
